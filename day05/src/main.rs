37c2583ec59d844689be86e4dbea05758302aa68
Andrea Corradi <andreac@unstable.it>
2019-03-27 22:11:26 +0100

    [day05] Add solution to day 5 part 1

---
+++ day05/src/main.rs
@@ -0,0 +1,65 @@
+use std::io::{self, Read};
+
+fn unit_react(a: char, b: char) -> bool {
+    a != b && a.eq_ignore_ascii_case(&b)
+}
+
+fn polymer_react(p: &str) -> Vec<char> {
+    p.lines().nth(0).expect("No line to parse").chars()
+        .fold(Vec::new(), |mut us, u| {
+            match us.last() {
+                Some(top) if unit_react(*top, u) => {
+                    us.pop();
+                }
+                _ => {
+                    us.push(u);
+                }
+            };
+
+            us
+        })
+}
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input)?;
+    let input = input;
+
+    let units_n = polymer_react(&input).len();
+
+    println!("{}", units_n);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{polymer_react, unit_react};
+
+    #[test]
+    fn test_do_react() {
+        assert!(unit_react('a', 'A'));
+        assert!(unit_react('A', 'a'));
+    }
+
+    #[test]
+    fn test_do_not_react() {
+        assert!(!unit_react('a', 'a'));
+        assert!(!unit_react('A', 'A'));
+        assert!(!unit_react('a', 'b'));
+        assert!(!unit_react('A', 'b'));
+        assert!(!unit_react('A', 'B'));
+        assert!(!unit_react('a', 'B'));
+    }
+
+    #[test]
+    fn test_polymer_react() {
+        let pr = |s: &str| polymer_react(&s).into_iter().collect::<String>();
+
+        assert_eq!(pr("aA"), "");
+        assert_eq!(pr("abBA"), "");
+        assert_eq!(pr("abAB"), "abAB");
+        assert_eq!(pr("aabAAB"), "aabAAB");
+        assert_eq!(pr("dabAcCaCBAcCcaDA"), "dabCBAcaDA");
+    }
+}
