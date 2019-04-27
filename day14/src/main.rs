1454af8e9f37509a00d470b02b56c65e730468ab
Andrea Corradi <andreac@unstable.it>
2019-04-27 18:45:13 +0200

    [day14] Use a generatator for the scoreboard

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,9 +1,11 @@
-use std::io::{self, Read};
 use std::collections::LinkedList;
+use std::io::{self, Read};
 
 fn get_digits(mut n: usize) -> Vec<u8> {
     let mut digits = Vec::with_capacity(2);
-    let mut push = |n| { digits.push((n%10) as u8); };
+    let mut push = |n| {
+        digits.push((n % 10) as u8);
+    };
 
     if n == 0 {
         push(n);
@@ -17,71 +19,84 @@ fn get_digits(mut n: usize) -> Vec<u8> {
     digits.into_iter().rev().collect()
 }
 
-// the scoreboard can be bigger then 'limit'
-// because we always add all the digits
-fn compute_scoreboard(limit: usize) -> Vec<u8> {
-    let mut scoreboard: Vec<u8> = Vec::with_capacity(limit * 2);
-    scoreboard.push(3);
-    scoreboard.push(7);
+struct Scoreboard {
+    scores: Vec<u8>,
+    a_idx: usize,
+    b_idx: usize,
+}
 
-    let mut a_idx = 0;
-    let mut b_idx = 1;
+struct ScoreboardIter<'a> {
+    scoreboard: &'a mut Scoreboard,
+    iter_pos: usize,
+}
 
-    while scoreboard.len() < limit {
-        let a_score = scoreboard[a_idx];
-        let b_score = scoreboard[b_idx];
-        let new_score = a_score + b_score;
-        let mut digits = get_digits(new_score as usize);
-        scoreboard.append(&mut digits);
-
-        a_idx = (a_idx + a_score as usize + 1) % scoreboard.len();
-        b_idx = (b_idx + b_score as usize + 1) % scoreboard.len();
+impl Scoreboard {
+    fn new() -> Scoreboard {
+        Scoreboard {
+            scores: vec![3, 7],
+            a_idx: 0,
+            b_idx: 1,
+        }
     }
 
-    scoreboard
+    fn len(&self) -> usize {
+        self.scores.len()
+    }
+
+    fn iter<'a>(&'a mut self) -> ScoreboardIter<'a> {
+        ScoreboardIter {
+            scoreboard: self,
+            iter_pos: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for ScoreboardIter<'a> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<u8> {
+        let a_idx = self.scoreboard.a_idx;
+        let b_idx = self.scoreboard.b_idx;
+
+        if self.iter_pos == self.scoreboard.len() {
+            let a_score = self.scoreboard.scores[a_idx];
+            let b_score = self.scoreboard.scores[b_idx];
+            let new_score = a_score + b_score;
+            let mut digits = get_digits(new_score as usize);
+            self.scoreboard.scores.append(&mut digits);
+
+            let len = self.scoreboard.len();
+            self.scoreboard.a_idx = (a_idx + a_score as usize + 1) % len;
+            self.scoreboard.b_idx = (b_idx + b_score as usize + 1) % len;
+        }
+
+        let digit = self.scoreboard.scores[self.iter_pos];
+        self.iter_pos += 1;
+
+        Some(digit)
+    }
 }
 
 fn next_10_scores(limit: usize) -> Vec<u8> {
-    compute_scoreboard(limit + 10)
-        .into_iter()
-        .skip(limit)
-        .take(10)
-        .collect()
+    Scoreboard::new().iter().skip(limit).take(10).collect()
 }
 
-fn find_in_scoreboard(target: &Vec<u8>) -> usize  {
-    let mut scoreboard: Vec<u8> = Vec::new();
-    scoreboard.push(3);
-    scoreboard.push(7);
+fn find_in_scoreboard(target: &Vec<u8>) -> usize {
+    let mut scoreboard = Scoreboard::new();
+    let mut window: LinkedList<u8> = scoreboard.iter().take(target.len()).collect();
 
-    let mut a_idx = 0;
-    let mut b_idx = 1;
-
-    let mut window: LinkedList<u8> = LinkedList::new();
+    let mut it = scoreboard.iter();
     loop {
-
-        let a_score = scoreboard[a_idx];
-        let b_score = scoreboard[b_idx];
-        let new_score = a_score + b_score;
-        let mut digits = get_digits(new_score as usize);
-        for (offset, &d) in digits.iter().enumerate() {
-            window.push_back(d);
-            if window.len() > target.len() {
-                window.pop_front();
-            }
-            if window.iter().enumerate().all(|(idx, &v)| target[idx] == v) {
-                return scoreboard.len() + offset + 1 - target.len();
-            }
+        if window.iter().enumerate().all(|(idx, &v)| target[idx] == v) {
+            return scoreboard.len() - target.len();
         }
 
-        scoreboard.append(&mut digits);
-
-        a_idx = (a_idx + a_score as usize + 1) % scoreboard.len();
-        b_idx = (b_idx + b_score as usize + 1) % scoreboard.len();
+        // the iterator will always return a score
+        let digit = it.next().unwrap();
+        window.pop_front();
+        window.push_back(digit);
     }
-
 }
-
 
 fn main() -> io::Result<()> {
     let mut input = String::new();
@@ -90,18 +105,20 @@ fn main() -> io::Result<()> {
 
     let limit = input.parse::<usize>().expect("Number of recipes");
 
-    next_10_scores(limit).iter().for_each(|score| { print!("{}", score); });
+    next_10_scores(limit).iter().for_each(|score| {
+        print!("{}", score);
+    });
     println!("");
 
     let digits = get_digits(limit);
-    println!("{:?} {}", digits, find_in_scoreboard(&digits));
+    println!("{}", find_in_scoreboard(&digits));
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{find_in_scoreboard, compute_scoreboard, get_digits, next_10_scores};
+    use super::{find_in_scoreboard, get_digits, next_10_scores, Scoreboard};
 
     #[test]
     fn test_get_digits() {
@@ -123,16 +140,19 @@ mod tests {
 
     #[test]
     fn test_compute_scoreboard() {
-        assert_eq!(compute_scoreboard(0), vec![3, 7]);
-        assert_eq!(compute_scoreboard(5), vec![3, 7, 1, 0, 1, 0]);
-        assert_eq!(compute_scoreboard(6), vec![3, 7, 1, 0, 1, 0]);
-        assert_eq!(compute_scoreboard(10), vec![3, 7, 1, 0, 1, 0, 1, 2, 4, 5]);
+        let scoreboard = |n| Scoreboard::new().iter().take(n).collect::<Vec<_>>();
+
+        assert_eq!(scoreboard(0), vec![]);
+        assert_eq!(scoreboard(2), vec![3, 7]);
+        assert_eq!(scoreboard(5), vec![3, 7, 1, 0, 1]);
+        assert_eq!(scoreboard(6), vec![3, 7, 1, 0, 1, 0]);
+        assert_eq!(scoreboard(10), vec![3, 7, 1, 0, 1, 0, 1, 2, 4, 5]);
         assert_eq!(
-            compute_scoreboard(14),
+            scoreboard(14),
             vec![3, 7, 1, 0, 1, 0, 1, 2, 4, 5, 1, 5, 8, 9]
         );
         assert_eq!(
-            compute_scoreboard(19),
+            scoreboard(19),
             vec![3, 7, 1, 0, 1, 0, 1, 2, 4, 5, 1, 5, 8, 9, 1, 6, 7, 7, 9]
         );
     }
@@ -154,9 +174,9 @@ mod tests {
 
     #[test]
     fn test_find_in_scoreboard() {
-        assert_eq!(find_in_scoreboard(&vec![5,1,5,8,9]), 9);
-        assert_eq!(find_in_scoreboard(&vec![0,1,2,4,5]), 5);
-        assert_eq!(find_in_scoreboard(&vec![9,2,5,1,0]), 18);
-        assert_eq!(find_in_scoreboard(&vec![5,9,4,1,4]), 2018);
+        assert_eq!(find_in_scoreboard(&vec![5, 1, 5, 8, 9]), 9);
+        assert_eq!(find_in_scoreboard(&vec![0, 1, 2, 4, 5]), 5);
+        assert_eq!(find_in_scoreboard(&vec![9, 2, 5, 1, 0]), 18);
+        assert_eq!(find_in_scoreboard(&vec![5, 9, 4, 1, 4]), 2018);
     }
 }
